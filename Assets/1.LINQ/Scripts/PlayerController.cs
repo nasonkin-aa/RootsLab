c193054776450a391a479bc188f48ea49a6d6bc8
nasonkin-aa <nasonkin-andrey@mail.ru>
2026-03-13 14:15:32 +1000

    Add LINQ

---
+++ Assets/1.LINQ/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    public Transform shootPoint;
+    public float shootInterval = 1f;
+
+    private float shootTimer = 0f; 
+
+    void Update()
+    {
+        shootTimer += Time.deltaTime;
+        if (shootTimer >= shootInterval)
+        {
+            ShootAtAllEnemies();
+            shootTimer = 0f;
+        }
+    }
+
+    void ShootAtAllEnemies()
+    {
+        var entitys = FindObjectsOfType<Entity>();
+
+
+    }
+
+   
+   
+    private void SpawnArrow(Transform enemy)
+    {
+        GameObject arrow = Instantiate(arrowPrefab, shootPoint.position, Quaternion.identity);
+        Arrow arrowScript = arrow.GetComponent<Arrow>();
+        arrowScript.SetTarget(enemy.transform);
+    }
+}
